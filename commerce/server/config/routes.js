b16ed82b2f6ff60bada4ea228c0f7e70ebfd66cd
Duc Dang <your@email.com>
2018-07-25 13:13:11 -0500

    commer2

--- commerce/server/config/routes.js
+++ commerce/server/config/routes.js
@@ -1,12 +1,16 @@
 var path = require("path"),
 	products = require("../controllers/products");
 
+    // routes are first checked here, in Express
+
 module.exports = function(app) {
 	app.get("/products", products.index);
 	app.post("/products", products.create);
 	app.get("/products/:id", products.get);
 	app.delete("/products/:id", products.delete);
-	app.patch("/products/:id", products.update);
+    app.patch("/products/:id", products.update);
+    
+    // if no Express routes match, go back to Angular
 	app.all("*", (req,res,next)=>{
 		res.sendFile(path.resolve("./client/dist/client/index.html"))
 	});
